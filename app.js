202699656b51da1f18318dac2d3a34c460757461
Garrett Ourth <garrettourth@gmail.com>
2023-05-04 00:00:23 -0500

    attempt to fix cors issue

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
+const cors = require('cors');
 
 const port = process.env.PORT || 2000;
 const app = express();
@@ -10,10 +11,14 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
   })
   // .use('/professional', professionalRoutes);
   .use('/', require('./routes'));
+
+  app.use(cors());
 
 mongodb.initDb((err, mongodb) => {
   if (err) {

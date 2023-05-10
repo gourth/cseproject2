a79568f4d9e0173836b14e1b86d5e04bdea45a4a
Garrett Ourth <garrettourth@gmail.com>
2023-05-10 13:04:58 -0500

    add test points, callback

--- app.js
+++ app.js
@@ -50,6 +50,24 @@ app
   }
   ));
 
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.get('/', (req, res) => {res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out")});
+
+app.get('/github/callback', passport.authenticate('github', {
+  failureRedirect: "/api-docs", session: false}),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect('/');
+  
+  });
+
 mongodb.initDb((err, mongodb) => {
   if (err) {
     console.log(err);

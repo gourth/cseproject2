e8dac587b03ed8a2da7858a2375fc4b0c9cda378
Garrett Ourth <garrettourth@gmail.com>
2023-05-10 12:59:16 -0500

    begin passport setup and add .env

--- app.js
+++ app.js
@@ -14,16 +14,41 @@ const app = express();
 
 app
   .use(bodyParser.json())
+  .use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  }))
+  // This is the basci expres session ({..}) initialization.
+  .use(passport.initialize())
+  // init passport on every route call.
+  .use(passport.session())
+  // allow passport to use "express-session".
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
   })
+  .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']}))
+  .use(cors({ origin: '*'}))
+  
   
   .use('/', require('./routes'));
 
   app.use(cors());
+
+  passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    //User.findOrCreate({ githubId: profile.id}, function (err, user) {
+      return done(null, profile);
+    //});
+  }
+  ));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {

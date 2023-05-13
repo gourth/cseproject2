dc38038a6fdea70587838a7b90a8ca5e5d53c909
Garrett Ourth <garrettourth@gmail.com>
2023-05-12 20:45:06 -0500

    add authentication

--- app.js
+++ app.js
@@ -63,13 +63,14 @@ passport.deserializeUser((user, done) => {
 });
 
 app.get('/', (req, res) => {res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out")});
-console.log("help me")
+
 
 app.get('/github/callback', passport.authenticate('github', {
   failureRedirect: "/api-docs", session: false}),
   (req, res) => {
     req.session.user = req.user;
     res.redirect('/');
+    console.log("help me");
   
   });
 
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,14 +3,16 @@ const router = express.Router();
 
 const pokemonController = require('../controllers/pokemon');
 
+const { isAuthenticated } = require("../middleware/authenticate");
+
 router.get('/', pokemonController.getAll);
 
 router.get('/:id', pokemonController.getSingle);
 
-router.post('/', pokemonController.createPokemon);
+router.post('/', isAuthenticated, pokemonController.createPokemon);
 
-router.put('/:id', pokemonController.updatePokemon);
+router.put('/:id', isAuthenticated, pokemonController.updatePokemon);
 
-router.delete('/:id', pokemonController.deletePokemon);
+router.delete('/:id', isAuthenticated, pokemonController.deletePokemon);
 
 module.exports = router;
